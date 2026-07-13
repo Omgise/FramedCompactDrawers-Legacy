b5a7ee28d2983f8c5aca68b8af9b5391778ec15e
Duncan Campbell <campbellduncan12@gmail.com>
2026-07-12 21:29:03 -0500

    Initial repo commit

--- src/main/java/com/mrfuzzihead/framedcompactdrawers/ClientProxy.java
+++ src/main/java/com/mrfuzzihead/framedcompactdrawers/ClientProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.mrfuzzihead.framedcompactdrawers;
 
 public class ClientProxy extends CommonProxy {
 
--- src/main/java/com/mrfuzzihead/framedcompactdrawers/Config.java
+++ src/main/java/com/mrfuzzihead/framedcompactdrawers/Config.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.mrfuzzihead.framedcompactdrawers;
 
 import java.io.File;
 
