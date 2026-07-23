bcc261721f4c9163d0a6f718ed5b445825b5332d
Duncan Campbell <campbellduncan12@gmail.com>
2026-07-22 20:52:44 -0500

    Fix mixins

--- src/main/java/com/mrfuzzihead/framedcompactdrawers/mixins/Mixins.java
+++ src/main/java/com/mrfuzzihead/framedcompactdrawers/mixins/Mixins.java
@@ -10,10 +10,8 @@ public enum Mixins implements IMixins {
     // Read the Javadoc of IMixins and MixinBuilder for further information
     // You should declare all of your mixins early and late in this same enum
     STORAGEDRAWERS(new MixinBuilder().setPhase(Phase.LATE)
-        .addCommonMixins(
-            "storagedrawers.MixinTileEntityFramingTable",
-            "storagedrawers.MixinContainerFramingTable",
-            "storagedrawers.MixinDrawersItemRenderer")
+        .addCommonMixins("storagedrawers.MixinTileEntityFramingTable", "storagedrawers.MixinContainerFramingTable")
+        .addClientMixins("storagedrawers.MixinDrawersItemRenderer")
         .addRequiredMod(TargetMods.STORAGEDRAWERS));
 
     private final MixinBuilder builder;
